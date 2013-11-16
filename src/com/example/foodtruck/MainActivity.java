6be5ae570c867e60c4eef51496556e24cfece2f8
U-Andrew-VAIO\Andrew <Andrew@Andrew-VAIO.(none)>
2013-11-16 18:13:52 -0500

    Added buttons on front page, created holder user locations view page

--- src/com/example/foodtruck/MainActivity.java
+++ src/com/example/foodtruck/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.foodtruck;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Application;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,24 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		// Go to Food Truck activity
+		
+		
+		// Go to user locations view activity
+		
+		Button goToUserLocationViewButton = (Button) findViewById(R.id.userViewButton);
+		goToUserLocationViewButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent launchUserLocationView = new Intent(getApplicationContext(), UserLocationsView.class);
+				startActivity(launchUserLocationView);
+			}});
+		
+		
 	}
 
 	@Override
