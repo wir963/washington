7c01ee1f089715b5860114732cdd8cf4ed4bb900
Welles Robinson <wir963@gmail.com>
2013-11-16 15:19:56 -0800

    welles added microsoft azure server stuff

--- src/com/example/foodtruck/MainActivity.java
+++ src/com/example/foodtruck/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.foodtruck;
 
+import java.net.MalformedURLException;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import com.microsoft.windowsazure.mobileservices.*;
 
 public class MainActivity extends Activity {
+	
+	private MobileServiceClient mClient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		try {
+			mClient = new MobileServiceClient("https://gettruckedup.azure-mobile.net/", "djHwmBvRUfXjnAYNCAnPawmlcHQNrx41", this);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -17,6 +28,20 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void exampleMethod() {
+		/*Item item = new Item();
+		item.Text = "Awesome item";
+		mClient.getTable(Item.class).insert(item, new TableOperationCallback<Item>() {
+		      public void onCompleted(Item entity, Exception exception, ServiceFilterResponse response) {
+		            if (exception == null) {
+		                  // Insert succeeded
+		            } else {
+		                  // Insert failed
+		            }
+		      }
+		});*/
 	}
 
 }
