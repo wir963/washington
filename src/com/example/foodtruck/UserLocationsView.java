bbd6387079f51ac5efef25ff2767848bd99c2eb4
Janet Zhu <janetzhuzhu@gmail.com>
2013-11-17 09:31:55 -0800

    fixed paypal button

--- src/com/example/foodtruck/UserLocationsView.java
+++ src/com/example/foodtruck/UserLocationsView.java
@@ -1,7 +1,10 @@
 package com.example.foodtruck;
 
+import java.math.BigDecimal;
 import java.net.MalformedURLException;
 import java.util.List;
+
+import org.json.JSONException;
 
 import android.location.Location;
 import android.location.LocationListener;
@@ -42,9 +45,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
 import com.microsoft.windowsazure.mobileservices.ServiceFilterResponse;
 import com.microsoft.windowsazure.mobileservices.TableQueryCallback;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
 public class UserLocationsView extends Activity implements LocationListener
 {
+	
+	private static final String CONFIG_ENVIRONMENT = PaymentActivity.ENVIRONMENT_NO_NETWORK;
+
+    private static final String CONFIG_CLIENT_ID = "AVZ0jRB7XveAdyS4VEkpY1-s4xUoLr-yo8hZ2U8pbGgM5vPW5EnYHzVBhGa-";
+
+    private static final String CONFIG_RECEIVER_EMAIL = ""; 
+    
 	GoogleMap map;
 	protected LocationListener locationListener;
 	protected LatLng myLatLng;
@@ -66,6 +80,14 @@ public class UserLocationsView extends Activity implements LocationListener
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		Intent intent = new Intent(this, PayPalService.class);
+
+	    // live: don't put any environment extra
+	    // sandbox: use PaymentActivity.ENVIRONMENT_SANDBOX
+	    intent.putExtra(PaymentActivity.EXTRA_PAYPAL_ENVIRONMENT, PaymentActivity.ENVIRONMENT_NO_NETWORK);
+	    intent.putExtra(PaymentActivity.EXTRA_CLIENT_ID, "AVZ0jRB7XveAdyS4VEkpY1-s4xUoLr-yo8hZ2U8pbGgM5vPW5EnYHzVBhGa-");
+	    startService(intent);
 		
 		//no android header
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -141,10 +163,8 @@ public class UserLocationsView extends Activity implements LocationListener
 				
 				String pledgeAmt = pledgeAmount.getText().toString();
 				
-				//Toast newToast = Toast.makeText(getApplicationContext(), username + password, Toast.LENGTH_LONG);
-				//newToast.show();
-    			Intent moveToMenuSelection = new Intent(getApplicationContext(), PaypalTransactionActivity.class); //will launch the PayPal Transaction app
-    			startActivity(moveToMenuSelection);
+				onBuyPressed(arg0);
+
 			}});
 		
 	}
@@ -206,6 +226,63 @@ public class UserLocationsView extends Activity implements LocationListener
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		Log.d("Latitude","status");
+	}
+	
+	
+	@Override
+	public void onDestroy() {
+	    stopService(new Intent(this, PayPalService.class));
+	    super.onDestroy();
+	}
+	
+	///PAYPAL STUFF
+	
+	public void onBuyPressed(View pressed) {
+		 PayPalPayment newPayment = new PayPalPayment(new BigDecimal(getIntent().getExtras().getString("amount")), "USD", "Pledge Amount");
+	        
+	        Intent intent = new Intent(this, PaymentActivity.class);
+	        
+	        intent.putExtra(PaymentActivity.EXTRA_PAYPAL_ENVIRONMENT, CONFIG_ENVIRONMENT);
+	        intent.putExtra(PaymentActivity.EXTRA_CLIENT_ID, CONFIG_CLIENT_ID);
+	        intent.putExtra(PaymentActivity.EXTRA_RECEIVER_EMAIL, CONFIG_RECEIVER_EMAIL);
+	        
+	        intent.putExtra(PaymentActivity.EXTRA_CLIENT_ID, "AVZ0jRB7XveAdyS4VEkpY1-s4xUoLr-yo8hZ2U8pbGgM5vPW5EnYHzVBhGa-");
+	        intent.putExtra(PaymentActivity.EXTRA_PAYER_ID, "your-customer-id-in-your-system");
+	        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, newPayment);
+	        
+	        startActivityForResult(intent, 0);
+	}
+
+	@Override
+	protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+	    if (resultCode == Activity.RESULT_OK) {
+	        PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+	        if (confirm != null) {
+	            try {
+	                Log.i("paymentExample", confirm.toJSONObject().toString(4));
+
+	                // TODO: send 'confirm' to your server for verification.
+	                // see https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+	                // for more details.
+	                
+	                
+
+	            } catch (JSONException e) {
+	                Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+	            }
+	        }
+	    }
+	    else if (resultCode == Activity.RESULT_CANCELED) {
+	        Log.i("paymentExample", "The user cancelled.");
+	    }
+	    else if (resultCode == PaymentActivity.RESULT_PAYMENT_INVALID) {
+	        Log.i("paymentExample", "An invalid payment was submitted. Please see the docs.");
+	    }
+	    
+	    
+		//Intent intent = new Intent(getApplicationContext(), PayConfirm.class); //will launch the menuSelection application
+		//startActivity(intent);
+		
 	}
 
 
