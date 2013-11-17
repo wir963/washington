d26b8966449ffaa653390ec79a90370cb414f8f1
U-Andrew-VAIO\Andrew <Andrew@Andrew-VAIO.(none)>
2013-11-16 19:27:41 -0500

    Still working on maps integration

--- src/com/example/foodtruck/UserLocationsView.java
+++ src/com/example/foodtruck/UserLocationsView.java
@@ -1,15 +1,28 @@
 package com.example.foodtruck;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class UserLocationsView extends Activity {
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_locations_view);
+		
+		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+        .getMap();
 	}
 
 	@Override
