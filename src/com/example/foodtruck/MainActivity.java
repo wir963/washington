0fa19ccbbc063acfcea0b844112011e06b3f7e43
Welles Robinson <wir963@gmail.com>
2013-11-17 13:01:14 -0800

    update truck link

--- src/com/example/foodtruck/MainActivity.java
+++ src/com/example/foodtruck/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				Intent launchTruckLocationView = new Intent(getApplicationContext(), StopSelectActivity.class);
+				Intent launchTruckLocationView = new Intent(getApplicationContext(), StopListViewMultipleSelectionActivity.class);
 				startActivity(launchTruckLocationView);
 			}});// end of onClickListener
 		
