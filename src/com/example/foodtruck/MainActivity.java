e44030c33f54dcdf23cd074b3c22106adc088cf2
Welles Robinson <wir963@gmail.com>
2013-11-16 17:09:53 -0800

    azure changes

--- src/com/example/foodtruck/MainActivity.java
+++ src/com/example/foodtruck/MainActivity.java
@@ -4,7 +4,6 @@ import java.net.MalformedURLException;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.Application;
 import android.content.Intent;
 import android.view.Menu;
 import com.microsoft.windowsazure.mobileservices.*;
